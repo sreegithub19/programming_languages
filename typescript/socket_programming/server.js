a720fd17fb5bab86cc8b5c9aa69e4590d9e1216e
K <sreedhar.k@rakuten.com>
2022-06-10 19:13:56 +0530

    Added files

--- typescript/socket_programming/server.js
+++ typescript/socket_programming/server.js
@@ -1,34 +1,61 @@
-// (working)
-const runner = require('child_process'); 
-let sprintf = require('sprintf-js').sprintf;
-var go_client_main = `
+  const runner = require('child_process'); 
+  let sprintf = require('sprintf-js').sprintf;
+  var go_server_main_1 = `
 import subprocess
 list_files_2 = subprocess.run(["python3","-c",'''
-import subprocess
-list_files_1 = subprocess.run(["goeval",\\'\\'\\'
-const (
-        SERVER_HOST = "localhost"
-        SERVER_PORT = "9989"
-        SERVER_TYPE = "tcp"
-    )
-    connection, err := net.Dial(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
-    if err != nil {
-        panic(err)
-    }
-    ///send some data
-    _, err = connection.Write([]byte("Hello Server! Greetings."))
-    buffer := make([]byte, 1024)
-    mLen, err := connection.Read(buffer)
-    if err != nil {
-        fmt.Println("Error reading:", err.Error())
-    }
-    fmt.Println("Received: ", string(buffer[:mLen]))
-    defer connection.Close()
+from subprocess import run
+list_files_1 = run(["goeval",\\'\\'\\'
+ const (
+         SERVER_HOST = "localhost"
+         SERVER_PORT = "9988"
+         SERVER_TYPE = "tcp"
+     )
+     processClient := func(connection net.Conn) {
+         buffer := make([]byte, 1024)
+         mLen, err := connection.Read(buffer)
+         if err != nil {
+             fmt.Println("Error reading:", err.Error())
+         }
+         fmt.Println("Received: ", string(buffer[:mLen]))
+         _, err = connection.Write([]byte("Thanks! Got your message:" + string(buffer[:mLen])))
+         connection.Close()
+     }
+     fmt.Println("Server Running...")
+     server, err := net.Listen(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
+     if err != nil {
+         fmt.Println("Error listening:", err.Error())
+         os.Exit(1)
+     }
+     
+     mains := func(){   
+         defer server.Close()      
+         fmt.Println("Listening on " + SERVER_HOST + ":" + SERVER_PORT)
+         fmt.Println("Waiting for client...")
+         for {
+             connection, err := server.Accept()
+             if err != nil {
+                 fmt.Println("Error accepting: ", err.Error())
+                 os.Exit(1)
+             }
+             fmt.Println("client connected")
+             go processClient(connection)
+         }
+     }   
+     
+     mains()
 \\'\\'\\'],timeout=500)
 print("The exit code was: %d" % list_files_1.returncode)
 '''],timeout=500)
 print("The exit code was: %d" % list_files_2.returncode)
+ `
+ runner.execFile("python3",["-c",go_server_main_1], (err, stdout, stderr) => { 
+     console.log(stdout) // hi 
+  });
+'''],timeout=500)
+print("The exit code was: %d" % list_files_3.returncode)
 `
-runner.execFile('python3',["-c",go_client_main], (err, stdout, stderr) => { 
+  
+
+  runner.execFile("python",["-c",go_server_main_1], (err, stdout, stderr) => { 
     console.log(stdout) // hi 
  });
