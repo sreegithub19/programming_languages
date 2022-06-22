6cd286a9e87010b5af3a41523db113ee92b433ad
K <sreedhar.k@rakuten.com>
2022-06-22 16:10:17 +0530

    Added changes

--- typescript/socket_programming/client.js
+++ typescript/socket_programming/client.js
@@ -1,32 +1,35 @@
 // // Node.js socket client script
+// https://mattsumme.rs/2015/nodejs-child-process-timeouts/
 const net = require('net');
 
 // Connect to a server @ port 9898
-const client = net.createConnection({ port: 9898 }, () => {
-  console.log('CLIENT: I connected to the server.');
-  client.write('CLIENT: Hello this is client!');
-  go_client();
-});
+// const client = net.createConnection({ port: 9898 }, () => {
+//   // console.log('CLIENT: I connected to the server.');
+//   // client.write('CLIENT: Hello this is client!');
+//   go_client();
+// });
 
-client.on('data', (data) => {
-  console.log(data.toString());
-  client.end();
-});
+// client.on('data', (data) => {
+//   console.log(data.toString());
+//   client.end();
+// });
 
-client.on('end', () => {
-  console.log('CLIENT: I disconnected from the server.');
-});
+// client.on('end', () => {
+//   console.log('CLIENT: I disconnected from the server.');
+// });
 
 // (working)
 
 //go_client();
+
+go_client();
 function go_client(){
 const runner = require('child_process'); 
 let sprintf = require('sprintf-js').sprintf;
 var go_client_main = `
 const (
         SERVER_HOST = "localhost"
-        SERVER_PORT = "9988"
+        SERVER_PORT = "9898"
         SERVER_TYPE = "tcp"
     )
     connection, err := net.Dial(SERVER_TYPE, SERVER_HOST+":"+SERVER_PORT)
@@ -46,5 +49,6 @@ const (
 runner.execFile('goeval',[go_client_main], (err, stdout, stderr) => { 
     console.log(stdout) // hi 
  });
+
 
 }
--- typescript/socket_programming/server.js
+++ typescript/socket_programming/server.js
@@ -1,47 +1,48 @@
 // // Node.js socket server script
+// https://mattsumme.rs/2015/nodejs-child-process-timeouts/
 const net = require('net');
 
-var globe = {};
+//var globe = {};
 // Create a server object
-const server = net.createServer((socket) => {
-  socket.on('data', (data) => {
-    console.log(data.toString());
+// const server = net.createServer((socket) => {
+//   socket.on('data', (data) => {
+//     console.log(data.toString());
     
-  });
+//   });
 
-  socket.write(`
-  SERVER: Hello! This is server speaking:
-  `);
-  socket.end('SERVER: Closing connection now.');
-}).on('error', (err) => {
-  console.error(err);
-});
+//   socket.write(`
+//   SERVER: Hello! This is server speaking:
+//   `);
+//   socket.end('SERVER: Closing connection now.');
+// }).on('error', (err) => {
+//   console.error(err);
+// });
 
-// Open server on port 9898
-server.listen(9898, () => {
-  console.log('opened server on', server.address().port);
+// // Open server on port 9898
+// server.listen(9898, () => {
+//   //console.log('opened server on', server.address().port);
   
+//   go_server();
+//   //go_server_1();
+// });
   go_server();
-  go_server_1();
-});
-
-function go_server_1(){
-  const runner = require('child_process'); 
-  let sprintf = require('sprintf-js').sprintf;
-  globe.val = `  
-   const (
-         SERVER_HOST = "localhost"
-         SERVER_PORT = "9988"
-         SERVER_TYPE = "tcp"
-     )
-         fmt.Println("Server Running...") 
-         fmt.Println("Listening on " + SERVER_HOST + ":" + SERVER_PORT)
-         fmt.Println("Waiting for client...")
- `
- runner.execFile("goeval",[globe.val], (err, stdout, stderr) => { 
-     console.log(stdout) // hi 
-  });
-}
+// function go_server_1(){
+//   const runner = require('child_process'); 
+//   let sprintf = require('sprintf-js').sprintf;
+//   globe.val = `  
+//    const (
+//          SERVER_HOST = "localhost"
+//          SERVER_PORT = "9988"
+//          SERVER_TYPE = "tcp"
+//      )
+//          fmt.Println("Server Running...") 
+//          fmt.Println("Listening on " + SERVER_HOST + ":" + SERVER_PORT)
+//          fmt.Println("Waiting for client...")
+//  `
+//  runner.execFile("goeval",[globe.val], (err, stdout, stderr) => { 
+//      console.log(stdout) // hi 
+//   });
+// }
 
 
 //go_server()
@@ -51,9 +52,14 @@ function go_server(){
   var go_server_main_1 = `
  const (
          SERVER_HOST = "localhost"
-         SERVER_PORT = "9988"
+         SERVER_PORT = "9898"
          SERVER_TYPE = "tcp"
      )
+
+    fmt.Println("Server Running...") 
+    fmt.Println("Listening on " + SERVER_HOST + ":" + SERVER_PORT)
+    fmt.Println("Waiting for client...")
+
      processClient := func(connection net.Conn) {
          buffer := make([]byte, 1024)
          mLen, err := connection.Read(buffer)
@@ -87,7 +93,11 @@ function go_server(){
      mains()
  `
 
- runner.execFile("goeval",[go_server_main_1], (err, stdout, stderr) => { 
+ var running = runner.execFile("goeval",[go_server_main_1], 
+  {timeout: 10000},   
+ (err, stdout, stderr) => { 
      console.log(stdout) // hi 
-  });
+  }
+  );
+
 }
