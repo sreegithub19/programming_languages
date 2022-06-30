bc1302af8dabebc1ab842827134fb297cd5d640b
K <sreedhar.k@rakuten.com>
2022-06-30 11:25:51 +0530

    Added files

--- Python/trials.py
+++ Python/trials.py
@@ -17,80 +17,88 @@
 # '''],timeout=500);
 # print("The exit code was: %d" % list_files_1.returncode);
 
+# import subprocess
+# list_files_1 = subprocess.run(["node","-e",
+# '''
+# //var exec = require("child_process").exec; 
+# const runner = require('child_process'); 
+# const phpString = ` 
+# echo "hi"; 
+# echo "hello"; 
+ 
+# echo '<!DOCTYPE html> 
+# <html> 
+# <body> 
+# <h2>An Unordered HTML List</h2> 
+# <ul> 
+#   <li>Coffee</li> 
+#   <li>Tea</li> 
+#   <li>Milk</li> 
+# </ul>   
+# <h2>An Ordered HTML List</h2> 
+# <script> 
+# alert(2); 
+# document.write("3+300000");   // this considers: 3+300000 (as int)  
+# document.write("<br>");     // this considers: "<br>" (as string) 
+# document.write(3+300000);   // this considers: 3+300000 (as int)  
+# alert("Hi");               // this considers: "Hi" (as string) 
+# document.write(2 === 3);       
+# </script> 
+# <ol> 
+#   <li>Coffee</li> 
+#   <li>Tea</li> 
+#   <li>Milk</li> 
+# </ol>  
+# </body> 
+# </html><br>'; 
+ 
+# $a = 2;                         // should put double-backslash before "$" to parse variables. 
+# echo $a; 
+# echo "<br>"; 
+# parse_str("first=value&arr[]=foo+bar&arr[]=baz", $output); 
+# echo $output['first'];  // value 
+# echo "\n"; 
+# echo $output['arr'][0]; // foo bar 
+# echo "\n"; 
+# echo $output['arr'][1]; // baz 
+# /******************************************************************/ 
+# /******************************************************************/ 
+ 
+# function my_callback($item) { 
+#   return strlen($item); 
+# } 
+ 
+# $strings = ["apple", "orange", "banana", "coconut"]; 
+# $lengths = array_map("my_callback", $strings); 
+# print_r($lengths); 
+ 
+# /******************************************************************/ 
+# ; 
+ 
+# ` // without <?php 
+# const express = require('express') 
+# const app = express() 
+# const port = 3000 
+ 
+# app.get('/', function(req, res){ 
+#   runner.execFile('php', ['-r', phpString], (err, stdout, stderr) => { 
+#     console.log(stdout) // hi 
+#     res.send(stdout); // hi 
+#  });}); 
+ 
+# app.listen(port, () => { 
+#   console.log(`Example app listening at http://localhost:${port}`) 
+# });
+# '''],timeout=500);
+# print("The exit code was: %d" % list_files_1.returncode);
+
+
 import subprocess
-list_files_1 = subprocess.run(["node","-e",
-'''
-//var exec = require("child_process").exec; 
-const runner = require('child_process'); 
-const phpString = ` 
-echo "hi"; 
-echo "hello"; 
- 
-echo '<!DOCTYPE html> 
-<html> 
-<body> 
-<h2>An Unordered HTML List</h2> 
-<ul> 
-  <li>Coffee</li> 
-  <li>Tea</li> 
-  <li>Milk</li> 
-</ul>   
-<h2>An Ordered HTML List</h2> 
-<script> 
-alert(2); 
-document.write("3+300000");   // this considers: 3+300000 (as int)  
-document.write("<br>");     // this considers: "<br>" (as string) 
-document.write(3+300000);   // this considers: 3+300000 (as int)  
-alert("Hi");               // this considers: "Hi" (as string) 
-document.write(2 === 3);       
-</script> 
-<ol> 
-  <li>Coffee</li> 
-  <li>Tea</li> 
-  <li>Milk</li> 
-</ol>  
-</body> 
-</html><br>'; 
- 
-$a = 2;                         // should put double-backslash before "$" to parse variables. 
-echo $a; 
-echo "<br>"; 
-parse_str("first=value&arr[]=foo+bar&arr[]=baz", $output); 
-echo $output['first'];  // value 
-echo "\n"; 
-echo $output['arr'][0]; // foo bar 
-echo "\n"; 
-echo $output['arr'][1]; // baz 
-/******************************************************************/ 
-/******************************************************************/ 
- 
-function my_callback($item) { 
-  return strlen($item); 
-} 
- 
-$strings = ["apple", "orange", "banana", "coconut"]; 
-$lengths = array_map("my_callback", $strings); 
-print_r($lengths); 
- 
-/******************************************************************/ 
-; 
- 
-` // without <?php 
-const express = require('express') 
-const app = express() 
-const port = 3000 
- 
-app.get('/', function(req, res){ 
-  runner.execFile('php', ['-r', phpString], (err, stdout, stderr) => { 
-    console.log(stdout) // hi 
-    res.send(stdout); // hi 
- });}); 
- 
-app.listen(port, () => { 
-  console.log(`Example app listening at http://localhost:${port}`) 
-});
-'''],timeout=500);
-print("The exit code was: %d" % list_files_1.returncode);
-
-
+list_files_1 = subprocess.run(["node","-e",'''
+var child_process = require('child_process');
+const prompt = require('prompt-sync')();
+var a = prompt("Enter a number:")
+console.log(a+2);
+'''],timeout=500)
+print("The exit code was: %d" % list_files_1.returncode)
 
--- typescript/input_in_child_process_not_working.js
+++ typescript/input_in_child_process_not_working.js
@@ -3,16 +3,20 @@ const prompt = require('prompt-sync')();
 
 // const input = prompt();
 // console.log(typeof(input))
-var a = "Hello"
-console.log(typeof(a))
-const python_string = `
-a = str(`+a+`)
-print(a)
-print("GoodBye!!",a)
-`
-child_process.execFile("python3",["-c",python_string], 
-{timeout:10000},
-(err, stdout, stderr) => { 
-    console.log(stdout)
- });
+//var a = "Hello"
+//console.log(typeof(a))
+//const python_string = `
+// #import sys
+// #a = sys.stdin.readline()
+// #print(a+"2")
+// #print("GoodBye!!",a)
+//`
+// child_process.execFile("python3",["-c",python_string], 
+// {timeout:10000},
+// (err, stdout, stderr) => { 
+//     console.log(stdout)
+//  });
 
+
+var a = prompt("Enter a number:")
+console.log(a+2);
