93149252c442310cf1e5ad6e946193da13efe00e
K <sreedhar.k@rakuten.com>
2022-06-30 06:40:47 +0530

    Added changes in file

--- typescript/input_in_child_process_not_working.js
+++ typescript/input_in_child_process_not_working.js
@@ -1,17 +1,15 @@
 var child_process = require('child_process');
 const prompt = require('prompt-sync')();
-var child_process = require('child_process');
 
-const input = prompt();
-console.log(typeof(input))
+// const input = prompt();
+// console.log(typeof(input))
 var a = "Hello"
-
-var python_string = `
-print("%s",${a})
-
+console.log(typeof(a))
+const python_string = `
+print("GoodBye!!")
 `
-child_process.exec("python",["-c",python_string], 
-//{timeout:10000},
+child_process.execFile("python3",["-c",python_string], 
+{timeout:10000},
 (err, stdout, stderr) => { 
     console.log(stdout)
  });
