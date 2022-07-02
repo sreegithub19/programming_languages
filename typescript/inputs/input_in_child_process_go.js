e38d2ba4d3db464379c93b9642117ef9fb12b7d0
sreegithub19 <cpadmaja2003@gmail.com>
2022-07-02 21:55:16 +0530

    Added changes

--- typescript/inputs/input_in_child_process_go.js
+++ typescript/inputs/input_in_child_process_go.js
@@ -1,19 +1,25 @@
- // working with workaround (file system)
+ // working with workaround (using file system)
 
 const prompt = require('prompt-sync')();
 const fs = require('fs');
 
-fs.writeFile('mynewfile1.txt', prompt("Enter first name:")+"\n", function (err) {});
-fs.appendFile('mynewfile1.txt', prompt("Enter last name:"), function (err) {});
-// works -> fs.readFile('mynewfile1.txt', function (err,data) {console.log(data.toString())});
+fs.writeFile('/tmp/text', prompt("Enter first name:")+"\n", function (err) {});
+fs.appendFile('/tmp/text', prompt("Enter last name:"), function (err) {});
+// works -> 
+  // fs.readFile('/tmp/text', function (err,data) {console.log(data.toString())});
 
 var p = require('child_process').execFile('goeval', [`
-data, err := ioutil.ReadFile("mynewfile1.txt")
+data, err := ioutil.ReadFile("/tmp/text")
     if err != nil {
         log.Panicf("failed reading data from file: \%s", err)
     }
     fmt.Printf("\\nSize: \%d bytes", len(data))
-    fmt.Printf("\\nData: \%s", data)
+    fmt.Printf("\\nData: \%s\\n", data)
+    //fmt.Println()
+    lines := strings.Split(string(data), "\\n")
+    for i, line := range lines {
+      fmt.Printf("\%d line: \%s\\n",i+1,line)
+    }
 `]
 ,
   {timeout: 10000},   
