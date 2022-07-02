f80c9e298cba22ba99e85970fb812cd8c3233184
sreegithub19 <cpadmaja2003@gmail.com>
2022-07-02 16:42:34 +0530

    Added changes

--- typescript/input_in_child_process_1.js
+++ typescript/input_in_child_process_1.js
@@ -1,10 +1,16 @@
- // use ${prompt()} instead of input() in child processes
+ // use str(${prompt()}) instead of input() for taking input in child processes
 
 const prompt = require('prompt-sync')();
 
 var p = require('child_process').execFile('python', ['-c',`
 a = str(${prompt("Enter a:")})
-print(int(a)+344)
+print(int(a)+344) 
+print(int(str(${prompt("Enter next:")}))+344)
+import subprocess
+list_files_1 = subprocess.run(["python","-c",'''\
+print("Input value in child process:",str(${prompt()}))
+'''],timeout=500)
+print("The exit code was: %d" % list_files_1.returncode)
 `]
 ,
 //  {timeout: 1000000},   
