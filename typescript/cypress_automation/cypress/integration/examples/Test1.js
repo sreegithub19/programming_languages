2a13059a0ecb1022017981cca0ba50bbe263151b
sreegithub19 <cpadmaja2003@gmail.com>
2022-07-09 20:28:16 +0530

    Added changes

---
+++ typescript/cypress_automation/cypress/integration/examples/Test1.js
@@ -0,0 +1,16 @@
+// cypress - Spec file (Test file)
+// Jasmine, Mocha - testing frameworks (Mocha is recommended and built in Cypress
+
+// https://www.rahulshettyacademy.com/seleniumPractise/#/
+describe('My First Test',function(){
+    it('Does not do much!'),function(){
+        //cy.visit("http://www.rahulshettyacademy.com/seleniumPractise/#/")
+        cy.visit("https://example.cypress.io")
+    }
+})
+
+
+
+
+
+
