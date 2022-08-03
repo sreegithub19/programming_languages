012bc71cd63db2d5f2738c8edd14d5cfc92b2725
sreegithub19 <cpadmaja2003@gmail.com>
2022-08-03 22:20:50 +0530

    Added changes

--- Python/mypy_1.py
+++ Python/mypy_1.py
@@ -1,8 +1,13 @@
-def fib(n: int) -> int:
-    if n <= 1:
-        return n
-    else:
-        return fib(n - 1) + fib(n - 2)
+# def fib(n: int) -> int:
+#     if n <= 1:
+#         return n
+#     else:
+#         return fib(n - 1) + fib(n - 2)
 
 
-print(fib(34))
+# print(fib(34))
+print(4^1)
+
+
+
+
