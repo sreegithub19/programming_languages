826f7bf54d0fccae49d684f7b86f216772ce0b37
K <sreedhar.k@rakuten.com>
2022-08-06 14:22:29 +0530

    Added changes

--- Python/flask/trials_2.py
+++ Python/flask/trials_2.py
@@ -7,13 +7,21 @@ app.debug = True  #  to activate debug mode
 @app.route('/')
 def home_page():
         import subprocess
-        list_files_1 = subprocess.run(["php","-r",
-        '''
-        echo "
-        Hello World!\n"
-        ;
-        '''],timeout=500);
-        return("The exit code was: %d" % list_files_1.returncode)
+        return('''
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>My First Web Page</h2>
+<p>My first paragraph.</p>
+
+<script>
+window.alert(5 + 6);
+</script>
+
+</body>
+</html> 
+        ''')
 
 
-app.run(host='0.0.0.0', port=8080)
+app.run(host='0.0.0.0', port=8084)
