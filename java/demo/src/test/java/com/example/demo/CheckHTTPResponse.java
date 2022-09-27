fa4d0211ef8bf3ab665cce332bfad1f157ba0d37
K <sreedhar.k@rakuten.com>
2022-09-27 14:05:30 +0530

    Changes

---
+++ java/demo/src/test/java/com/example/demo/CheckHTTPResponse.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CheckHTTPResponse {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void shouldPassIfStringMatches(){
+        Assert.assertEquals(
+                "Hello World from Spring Boot",
+                testRestTemplate.getForObject("http://localhost:"+port+"/",String.class));
+    }
+}
