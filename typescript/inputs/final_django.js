b41e4802463c96b8221160ec27cef16cb32d2510
K <sreedhar.k@rakuten.com>
2022-10-26 09:00:42 +0530

    C

--- typescript/inputs/final_django.js
+++ typescript/inputs/final_django.js
@@ -1,11 +1,19 @@
 // pip install Django==2.2.16
 
-var {PythonShell} = require('python-shell'); 
+// https://www.reddit.com/r/node/comments/at8uoi/how_to_call_python_program_with_std_input_in_node/
+var ps = require('python-shell'); 
 const open = require('open'); 
 const concurrently = require('concurrently'); 
 const { spawn, execFile } = require('child_process');
 
-var child = PythonShell.runString(`
+let options = ps.options;
+let PythonShell = ps.PythonShell;
+options =  {
+    'stdio':
+        ['pipe', 'pipe', 'pipe', 'pipe'] // stdin, stdout, stderr, custom
+}
+
+var child = `
 import sys      
 import pandas 
 from django.conf import settings 
@@ -18,6 +26,7 @@ settings.configure(
  ROOT_URLCONF=__name__, 
 ) 
  
+#print(input("Enter:"))
 def home(request): 
  return HttpResponse("<h1>Welcome 7000!</h1>") 
 def next(request): 
@@ -38,16 +47,21 @@ from django.core.management import execute_from_command_line
 
 execute_from_command_line([sys.argv[0], 'runserver','7000'])  # to change port number
 
-`);
+`
 
-if(!child)
-console.log("Not Working"); 
-else { 
-    console.log("Working"); 
+// if(!PythonShell.runString(child))
+// console.log("Not Working"); 
+// else { 
+//     console.log("Working"); 
+// }
 
+PythonShell.runString(child, options, function (err) {
+  if (err) throw err;
+  else console.log('finished');
+});
 
-    // // if below is commented or uncommented, and above is PythonShell.runString(), then Django server runs continuously (without being able to take any input)
-    // // if below is commented , and above is .execFile("python","-c","..."), then the Python script will only take input, Django server will not run (will show the "extra argument required" error)
+// // if below is commented or uncommented, and above is PythonShell.runString(), then Django server runs continuously (without being able to take any input)
+    // // if below is uncommented , and above is .execFile("python","-c","..."), then the Python script will only take input, Django server will not run (will show the "extra argument required" error)
     // child.stdout.pipe(process.stdout);
     // child.stderr.pipe(process.stderr);
     // process.stdin.pipe(child.stdin);
@@ -56,6 +70,3 @@ else {
     //         process.exit(); 
     //     //}, 20000);
     // });
-
-
-} 
