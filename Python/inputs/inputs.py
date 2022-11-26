18f3e0446eadee21e55c2eda32739e5af0b058f0
K <sreedhar.k@rakuten.com>
2022-11-26 09:53:05 +0530

    c

--- Python/inputs/inputs.py
+++ Python/inputs/inputs.py
@@ -1,20 +1,28 @@
-import subprocess
-import sys
-list_files_1 = subprocess.run(["python","-c",'''
-print(input("Enter a number:"))
-''']);
-print("The exit code was: %d" % list_files_1.returncode);
-print(list_files_1.stdout);
-print(list_files_1.stderr);
+# import subprocess
+# import sys
+# list_files_1 = subprocess.run(["python","-c",'''
+# print(input("Enter a number:"))
+# ''']);
+# print("The exit code was: %d" % list_files_1.returncode);
+# print(list_files_1.stdout);
+# print(list_files_1.stderr);
 
 
-list_files_1 = subprocess.run(["python3","-c",'''
-import subprocess
-import sys
+# list_files_1 = subprocess.run(["python3","-c",'''
+# import subprocess
+# import sys
 
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])   
-install("numpy")
-'''],check=True, capture_output=True)
-print(list_files_1.stdout)
-print(list_files_1.stderr)
+# def install(package):
+#     subprocess.check_call([sys.executable, "-m", "pip", "install", package])   
+# install("numpy")
+# '''],check=True, capture_output=True)
+# print(list_files_1.stdout)
+# print(list_files_1.stderr)
+
+import os
+
+os.system('''
+#!/bin/sh
+AGE=25; echo $AGE; AGE=35; echo $AGE
+'''
+)
---
+++ shell/one.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+AGE=25; echo $AGE; AGE=35; echo $AGE
