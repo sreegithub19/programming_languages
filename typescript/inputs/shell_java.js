972ff8516014aa6a92a41663d4f70a6deab3900f
K <sreedhar.k@rakuten.com>
2023-01-18 22:17:36 +0530

    Message

---
+++ typescript/inputs/shell_java.js
@@ -0,0 +1,79 @@
+const { spawn,exec } = require('child_process');
+
+
+var java_code = `'
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+
+  public static void main(String args[]) {
+
+    // Creates a string
+    String name = "Programiz";
+    System.out.println("String is: " + name);
+
+    try {
+
+      InputStream stream = new ByteArrayInputStream(name.getBytes(StandardCharsets.UTF_8));
+      System.out.println("InputStream: " + stream);
+
+      // Returns the available number of bytes
+      System.out.println("Available bytes at the beginning: " + stream.available());
+
+      // Reads 3 bytes from the stream stream
+      stream.read();
+      stream.read();
+      stream.read();
+
+      // After reading 3 bytes
+      // Returns the available number of bytes
+      System.out.println("Available bytes at the end: " + stream.available());
+
+      stream.close();
+    }
+
+    catch (Exception e) {
+      e.getStackTrace();
+    }
+  }
+}
+
+'`;
+
+var java_code1 = `'
+import java.util.Scanner;
+import java.io.LineNumberReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+      public static void main(String[] args) throws IOException {
+    System.out.println("enter one char");
+    char c = (char) System.in.read();
+    System.out.println("The char entered is :" + c);
+    String userInput;
+    System.out.println("Enter a string");
+    Scanner s = new Scanner(System.in);
+    userInput = s.next();
+    System.out.println("the string inputted is:" + userInput);
+}
+}
+
+'`;
+
+
+var child3 = exec(`
+cd /Users/sreedhar.k &&
+echo ${java_code} > /tmp/TmpClass.java && java /tmp/TmpClass.java &&
+echo ${java_code1} > /tmp/TmpClass.java && java /tmp/TmpClass.java
+
+`);
+
+child3.stdout.pipe(process.stdout);
+child3.stderr.pipe(process.stderr);
+process.stdin.pipe(child3.stdin);
+child3.on('exit', () => process.exit());
