3875c294e930a12f18108172d88a430cace69a6b
sreegithub19 <cpadmaja2003@gmail.com>
2023-02-23 00:58:21 +0530

    Message

--- javascript/templates_/ejs_/ejs_.js
+++ javascript/templates_/ejs_/ejs_.js
@@ -2,6 +2,14 @@
 // https://ejs.co/#install
 
 let ejs = require('ejs');
-let people = ['geddy', 'neil', 'alex'];
-let html = ejs.render('<%= people.join(", "); %>', {people: people});
+var fs = require('fs');
+
+// from string
+var people = ['geddy', 'neil', 'alex'];
+var html = ejs.render('<%= people.join(", "); %>', {people: people});
+console.log(html)
+
+// from file
+people = ['geddy', 'neil', 'alex'];
+html = ejs.render(fs.readFileSync("ejs_.ejs",'utf8').toString(), {people: people});
 console.log(html)
--- javascript/templates_/stylus_/readme.md
+++ javascript/templates_/stylus_/readme.md
@@ -1,5 +1,6 @@
 - npm install -g stylus
-- stylus -w style.styl -o style.css
-- stylus style.styl style.css && open index.html
+- stylus -w style.styl -o style.css   (to create the .css file)
+- stylus style.styl style.css && open index.html (Mac)
+- stylus style.styl style.css ; start index.html (Windows)
 
 - Reference: https://codepen.io/sheriffderek/pen/PjoqPm?editors=1100
