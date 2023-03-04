d60da6c6d8335e2a80b688b61bf442eb138a42aa
K <sreedhar.k@rakuten.com>
2023-03-04 10:27:16 +0530

    Message

---
+++ java/springboot_starter/src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloWorldController
+{
+    @RequestMapping("/")
+    public String hello()
+    {
+        return "Hello javaTpoint";
+    }
+}
