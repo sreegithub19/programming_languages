05bf90231b7d91547d178531f04eb8347b0a59c3
K <sreedhar.k@rakuten.com>
2023-03-05 23:10:03 +0530

    Message

--- java/java_mysql/src/javaMysql.java
+++ java/java_mysql/src/javaMysql.java
@@ -7,6 +7,7 @@ public class javaMysql{
     static final String USER = "root";
     static final String PASS = "sree@sql2021";
     static final String QUERY = "SELECT * FROM EEngineeringStudents";
+    static final String QUERY1 = "SELECT * FROM EEngineeringStudents";
 
     public static void main(String[] args) {
         // Open a connection
@@ -15,6 +16,25 @@ public class javaMysql{
             ResultSet rs = stmt.executeQuery(QUERY);) {
             // Extract data from result set
             while (rs.next()) {
+                System.out.println("rs:"+rs);
+                        // Retrieve by column name
+                System.out.print("Student_ID: " + rs.getInt("Student_ID"));
+                System.out.print(", Department: " + rs.getString("Department"));
+                System.out.print(", First_Name: " + rs.getString("First_Name"));
+                System.out.println(", Last_Name: " + rs.getString("Last_Name"));
+                System.out.println(", PassOutYear: " + rs.getInt("PassOutYear"));
+                System.out.println(", UniversityRank: " + rs.getInt("UniversityRank"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(QUERY1);) {
+            // Extract data from result set
+            while (rs.next()) {
+                System.out.println("rs1:"+rs);
                 // Retrieve by column name
                 System.out.print("Student_ID: " + rs.getInt("Student_ID"));
                 System.out.print(", Department: " + rs.getString("Department"));
