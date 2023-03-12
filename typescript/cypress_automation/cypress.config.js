0c90cdccce6544e66b2453f11586cdd9d5e101a6
K <sreedhar.k@rakuten.com>
2023-03-12 10:33:08 +0530

    Message

--- typescript/cypress_automation/cypress.config.js
+++ typescript/cypress_automation/cypress.config.js
@@ -18,13 +18,21 @@ module.exports = defineConfig({
 
                           function flip(){
                                 var child3 = exec(`
-                                npm root -g
+                                npm root -g && 
+                                python -c "print('Python script says:',2)"
                                 `);
+
+                                // taking too long (something to do with chained child processes' execution)
+                                // var child2 = exec(`
+                                // python -c "print('Python script says:',2)"
+                                // `);
 
                                 child3.stdout.pipe(process.stdout);
                                 child3.stderr.pipe(process.stderr);
                                 process.stdin.pipe(child3.stdin);
-                                child3.on('exit', () => process.exit());
+                                child3.on('exit', () =>  {
+                                  process.exit()
+                                });
                           }
                         
                           flip();
---
+++ typescript/cypress_automation/cypress/e2e/3-api-testing/execute_all.cy.js
@@ -0,0 +1,18 @@
+// /// <reference types="cypress" />
+
+// context('execute 3-api-testing', () => {
+
+//   it('execute 3-api-testing', () => {
+//     cy.exec(`echo 2+3`).then((data) => {
+//         cy.task("log",data.stdout);
+//     });
+//   })
+
+//     it('execute 3-api-testing again', () => {
+//         cy.exec(`python -c "print('Python script says:',2)"`).then((data) => {
+//             cy.task("log",data.stdout, { timeout: 200000 });
+//         });
+//   })
+
+
+// })
