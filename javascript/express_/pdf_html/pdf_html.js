8f64a4d047aaf48a5976981ff3ffbaa0b91f4fc7
K <sreedhar.k@rakuten.com>
2023-07-24 21:10:11 +0530

    Message

--- javascript/express_/pdf_html/pdf_html.js
+++ javascript/express_/pdf_html/pdf_html.js
@@ -1,36 +1,31 @@
-// Code to convert pdf to html: (Pdfcrowd Error: 458 - Conversion Time Exceeded)
-  // https://pdfcrowd.com/api/pdf-to-html-nodejs/
-
-  // encyclopedia_1.pdf: https://www.pdfdrive.com/world-encyclopedia-of-dinosaurs-prehistoric-creatures-the-ultimate-visual-reference-to-1000-dinosaurs-and-prehistoric-creatures-of-land-air-and-sea-from-the-triassic-jurassic-and-cretaceous-eras-d187762457.html
-  // encyclopedia.pdf: https://www.pdfdrive.com/encyclopedia-of-dinosaurs-e46112175.html
-  // the above html and pdf files exceeding git file size limit.
-
-// Website to convert pdf to html: https://pdf.io/pdf2html/
-
-/*
-// var pdfcrowd = require("pdfcrowd");
-
-// // create the API client instance
-// var client = new pdfcrowd.PdfToHtmlClient("demo", "ce544b6ea52a5621fb9d55f8b542d14d");
-
-// // run the conversion and write the result to a file
-// client.convertFileToFile(
-//     "C:/Users/Padmaja/Desktop/sreedhar/new_practice/programming_languages/javascript/express_/pdf_html/encyclopedia_1.pdf",
-//     "encyclopedia.html",
-//     function(err, fileName) {
-//         if (err) return console.error("Pdfcrowd Error: " + err);
-//         console.log("Success: the file was created " + fileName);
-//     });
-*/
 
 const express = require('express');
 const path = require('path');
+var public = path.join(__dirname, 'public');
+
 const app = express();
 // http://127.0.0.1:5000/
 
-app.get('/',function(req,res) {
+app.use("/",express.static('public'));
+
+
+// app.get('/',function(req,res) {
+//   res.send(` 
+//   <embed src="https://afgjilibrary.files.wordpress.com/2020/07/how-i-taught-my-grandmother-to-read-and-other-stories-pdfdrive.com-1.pdf" width="100%" height="1500px" />
+//   <hr>
+//   <embed src="http://www.4newmum.com/[Dorling_Kindersley]_Encyclopedia_of_Dinosaurs_and(BookFi).pdf" width="100%" height="1500px" />
+//   <hr>
+//   <embed src="https://ia904509.us.archive.org/18/items/visual-encyclopedia-by-dk-z-lib.org_202109/Visual%20Encyclopedia%20by%20DK%20%28z-lib.org%29.pdf" width="100%" height="1500px" />
+//   <hr>
+//   <embed src="http://giove.isti.cnr.it/demo/eread/Libri/joy/TSawyer.pdf" width="100%" height="1500px" />
+//   <hr>
+//   <embed src="https://www.gutenberg.org/cache/epub/76/pg76-images.html" width="90%" height="1500px" />
+//   `);
+// });
+
+app.get('/next',function(req,res) {
   //res.sendFile(__dirname + '/encyclopedia.html');
-  res.send(``);
+  res.sendFile(path.join(public, 'TS_and_JS_products.pdf'));
 });
 
 
