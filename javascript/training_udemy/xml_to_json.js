c2e126f08652721b043e4c28e7e45a2ecbb4d451
K <sreedhar.k@rakuten.com>
2023-10-28 12:49:41 +0530

    Message

---
+++ javascript/training_udemy/xml_to_json.js
@@ -0,0 +1,11 @@
+var convert = require('xml-js');
+var xml =
+'<?xml version="1.0" encoding="utf-8"?>' +
+'<note importance="high" logged="true">' +
+'    <title>Happy</title>' +
+'    <todo>Work</todo>' +
+'    <todo>Play</todo>' +
+'</note>';
+var result1 = convert.xml2json(xml, {compact: true, spaces: 4});
+var result2 = convert.xml2json(xml, {compact: false, spaces: 4});
+console.log(result1, '\n', result2);
