daf86b9bd6c7f3bfde8a0d18427f99d7c990a9c3
Sreedhar K <sreedhar.k@rakuten.com>
2025-01-18 12:43:13 +0530

    Message

--- assembly_/hello.asm
+++ assembly_/hello.asm
@@ -1,30 +1,18 @@
- ; Hello world assembly program
- ;                          
- ; Author: Pablo Corbalán   
- ; GitHub: https://github.com/pablocorbalann
- ; Date: 06/12/2020         
- ;
- ; Compiling the program    
- ; For compiling this hello world program you can use nasm and ld 
- ;
- ;   $ nasm -f elf32 -o hello.o hello.asm
- ;   $ ld -m elf_i386 -o hello hello.o 
- ;   $ ./hello
+section .data
+    hello db 'Hello world!', 0  ; Null-terminated string
 
-section        .text                   ; declare the .text section
-global         _start                  ; has to be declared for the linker (ld)
-_start:                                ; entry point for _start
-    mov edx, len                       ; "invoke" the len of the message
-    mov ecx, msg                       ; "invoke" the message itself
+section .text
+    global _start                ; Entry point for the linker
 
-    mov ebx, 1                         ; set the file descriptor (fd) to stdout
+_start:
+    ; Write the string to stdout
+    mov rax, 1                   ; sys_write system call number (1 = write)
+    mov rdi, 1                   ; file descriptor (1 = stdout)
+    mov rsi, hello               ; pointer to the string
+    mov rdx, 13                  ; length of the string
+    syscall                      ; invoke the system call
 
-    mov eax, 4                         ; system call for "write"   
-    int 0x80                           ; call the kernel
-
-    mov eax, 1                         ; system call for "exit"
-    int 0x80                           ; call the kernel
-
-section        .data                   ; here you declare the data
-    msg        db "Hello world!"       ; the actual message to use
-    len        equ $ -msg              ; get the size of the message
+    ; Exit the program
+    mov rax, 60                  ; sys_exit system call number
+    xor rdi, rdi                 ; exit status (0)
+    syscall                      ; invoke the system call
