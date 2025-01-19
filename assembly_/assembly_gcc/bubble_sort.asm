8b09723899e0cac1887fec7a2209d94337ffa6b3
Sreedhar K <sreedhar.k@rakuten.com>
2025-01-19 15:49:27 +0530

    Message

--- assembly_/assembly_gcc/bubble_sort.asm
+++ assembly_/assembly_gcc/bubble_sort.asm
@@ -1,25 +1,22 @@
-segment .data
-a: dq 2
-b: dq 0
-ind: dq 0
-cnt: dq 0
-cnt2: dq 0
-fmt: dq "%lld ",10,0
-fmt_out: dq "The 3rd largest number is: %lld ", 10, 0
+section .data
+    a: dq 2
+    b: dq 0
+    ind: dq 0
+    cnt: dq 0
+    cnt2: dq 0
+    fmt: db "%lld ", 10, 0
+    fmt_out: db "The 3rd largest number is: %lld ", 10, 0
 
-segment .bss
-array resq 21
+section .bss
+    array resq 21
 
-segment .text
-global main
-extern printf
+section .text
+    global main
+    extern printf
 
 main:
     push rbp
-
-    mov rax, 0
-    mov rcx, 0
-    mov rbx, 0
+    mov rbp, rsp
 
     ; Hardcode the five numbers into the array
     mov qword [array+0*8], 7
@@ -29,7 +26,6 @@ main:
     mov qword [array+4*8], 6
 
     mov rcx, 0
-    mov rbx, 5
 
 OUTER_LOOP:
     cmp rcx, 5
@@ -41,8 +37,8 @@ INNER_LOOP:
     inc rcx
     cmp rcx, 5
     jz OK 
-    cmp rax, [array+rcx*8]        
-    jle INNER_LOOP        
+    cmp rax, [array+rcx*8]
+    jle INNER_LOOP
     xchg rax, [array+rcx*8]
     jmp INNER_LOOP
 
@@ -54,14 +50,13 @@ OK:
 
 PRINT:
     mov rax, 0
-    mov rbx, 0
     mov rcx, 2
     mov rax, [array+rcx*8]
     mov rdi, fmt_out
     mov rsi, rax
-    call printf    
+    call printf
 
 END:
     mov rax, 0
-    pop rbp
+    leave
     ret
