15d153054ac75151a5551b97b6ab19b05b712b97
Sreedhar K <sreedhar.k@rakuten.com>
2025-01-19 14:43:06 +0530

    Message

--- assembly_/python_.asm
+++ assembly_/python_.asm
@@ -1,5 +1,5 @@
 section .data
-    python_command db 'python', 0
+    python_command db '/usr/bin/python3', 0
     arg1 db '-c', 0
     arg2 db 'print("Hello from Python")', 0
 
@@ -11,24 +11,24 @@ section .text
 
 _start:
     ; Prepare arguments for execve
-    mov eax, 11               ; sys_execve syscall number
-    lea ebx, [python_command] ; pointer to the command
-    lea ecx, [arg1]           ; argv[0] = -c
-    lea edx, [arg2]           ; argv[1] = 'print("Hello from Python")'
+    mov rax, 59               ; sys_execve syscall number
+    lea rdi, [rel python_command] ; pointer to the command
+    lea rsi, [rel arg1]           ; argv[0] = -c
+    lea rdx, [rel arg2]           ; argv[1] = 'print("Hello from Python")'
+
+    ; Setup stack for argv
+    mov r10, 0                ; null terminator
+    push r10                  ; argv[2] = NULL
+    push rdx                  ; argv[1] = arg2
+    push rsi                  ; argv[0] = arg1
+    push rdi                  ; argv[0] = python_command
+
+    mov rsi, rsp              ; argv = stack pointer
     
-    ; Push nullbyte to the stack
-    lea esi, [nullbyte]
-    push esi                  ; argv[2] = NULL
-    push edx                  ; argv[1] = arg2
-    push ecx                  ; argv[0] = arg1
-    push ebx                  ; argv[0] = python
-    
-    mov ecx, esp              ; argv = stack pointer
-    
-    xor edx, edx              ; envp = null
-    int 0x80                  ; make syscall
+    xor rdx, rdx              ; envp = null
+    syscall                   ; make syscall
 
     ; Exit the program
-    mov eax, 1                ; sys_exit syscall number
-    xor ebx, ebx              ; exit status 0
-    int 0x80                  ; make syscall
+    mov rax, 60               ; sys_exit syscall number
+    xor rdi, rdi              ; exit status 0
+    syscall                   ; make syscall
