df58db1fc6b92bd760f13d2f4a72707b6c2bdb61
Sreedhar K <sreedhar.k@rakuten.com>
2025-01-19 15:43:19 +0530

    Message

---
+++ assembly_/assembly_gcc/bubble_sort.asm
@@ -0,0 +1,67 @@
+segment .data
+a: dq 2
+b: dq 0
+ind: dq 0
+cnt: dq 0
+cnt2: dq 0
+fmt: dq "%lld ",10,0
+fmt_out: dq "The 3rd largest number is: %lld ", 10, 0
+
+segment .bss
+array resq 21
+
+segment .text
+global main
+extern printf
+
+main:
+    push rbp
+
+    mov rax, 0
+    mov rcx, 0
+    mov rbx, 0
+
+    ; Hardcode the five numbers into the array
+    mov qword [array+0*8], 7
+    mov qword [array+1*8], 13
+    mov qword [array+2*8], 2
+    mov qword [array+3*8], 10
+    mov qword [array+4*8], 6
+
+    mov rcx, 0
+    mov rbx, 5
+
+OUTER_LOOP:
+    cmp rcx, 5
+    jge PRINT
+    mov [cnt], rcx
+    mov rax, [array+rcx*8]
+
+INNER_LOOP:
+    inc rcx
+    cmp rcx, 5
+    jz OK 
+    cmp rax, [array+rcx*8]        
+    jle INNER_LOOP        
+    xchg rax, [array+rcx*8]
+    jmp INNER_LOOP
+
+OK:
+    mov rcx, [cnt]
+    mov [array+rcx*8], rax
+    inc rcx
+    jmp OUTER_LOOP
+
+PRINT:
+    mov rax, 0
+    mov rbx, 0
+    mov rcx, 2
+    mov rax, [array+rcx*8]
+    mov rdi, fmt_out
+    mov rsi, rax
+    call printf    
+
+END:
+    mov rax, 0
+    pop rbp
+    ret
