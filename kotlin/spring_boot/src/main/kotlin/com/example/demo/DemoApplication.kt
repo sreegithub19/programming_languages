eecafbfa3a19548d03185a04aea8806bfa84a8a1
Sreedhar K <sreedhar.k@rakuten.com>
2025-04-25 10:52:25 +0530

    Message

--- kotlin/spring_boot/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ kotlin/spring_boot/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DemoApplication
+open class DemoApplication
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
--- kotlin/spring_boot/src/main/kotlin/com/example/demo/controller/HelloController.kt
+++ kotlin/spring_boot/src/main/kotlin/com/example/demo/controller/HelloController.kt
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HelloController {
 
+    // http://localhost:8080/hello?name=test
     @GetMapping("/hello")
     fun sayHello(@RequestParam name: String): String {
         return "Hello, $name!"
