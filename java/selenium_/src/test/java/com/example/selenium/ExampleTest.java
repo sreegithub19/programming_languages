5d23060d5237c688bcea97ae2cf49d810053e66d
Sreedhar K <sreedhar.k@rakuten.com>
2025-04-29 10:30:55 +0530

    Message

--- java/selenium_/src/test/java/com/example/selenium/ExampleTest.java
+++ java/selenium_/src/test/java/com/example/selenium/ExampleTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
