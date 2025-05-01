a6a02bd56617e748489578b72ae970f53ed90e20
Sreedhar K <sreedhar.k@rakuten.com>
2025-05-01 16:15:49 +0530

    Message

--- kotlin/kotlin_js/src/main/kotlin/main.kt
+++ kotlin/kotlin_js/src/main/kotlin/main.kt
@@ -2,7 +2,9 @@ import kotlinx.browser.document
 
 fun main() {
     // Use document.write to add content to the webpage
-    document.write("<h1>Welcome to Kotlin/JS!</h1>")
-    document.write("<p>This content is written using Kotlin/JS and document.write.</p>")
-    document.write("<button onclick='alert(\"Hello from Kotlin/JS!\")'>Click Me</button>")
+    document.write("""
+    <h1>Welcome to Kotlin/JS!</h1>
+    <p>This content is written using Kotlin/JS and document.write.</p>
+    <button onclick='alert("Hello from Kotlin/JS!")'>Click Me</button>
+""")
 }
