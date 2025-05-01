500aa05a2e7f480148c49e38bfaaef431a831096
Sreedhar K <sreedhar.k@rakuten.com>
2025-05-01 12:31:52 +0530

    Message

--- kotlin/kotlin_js/build.gradle.kts
+++ kotlin/kotlin_js/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     kotlin("js") version "1.8.22" // Use the Kotlin/JS plugin
 }
 
+repositories {
+    mavenCentral() // Add Maven Central repository
+}
+
 kotlin {
     js(IR) {
         browser {
