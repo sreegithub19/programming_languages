03633e24099dc0323faa9886b90b6d936d26e342
sreegithub19 <cpadmaja2003@gmail.com>
2025-06-17 22:46:12 +0530

    Message

--- java/angular_spring_boot/frontend/src/app/app.module.ts
+++ java/angular_spring_boot/frontend/src/app/app.module.ts
@@ -1,14 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
+  // declarations: [AppComponent],
+  imports: [BrowserModule, AppComponent,HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
