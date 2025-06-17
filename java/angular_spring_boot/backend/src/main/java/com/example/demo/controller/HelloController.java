45f84a8f9f6e53f4447611c5ae9d76eb9929887a
sreegithub19 <cpadmaja2003@gmail.com>
2025-06-17 22:40:09 +0530

    Message

--- java/angular_spring_boot/backend/src/main/java/com/example/demo/controller/HelloController.java
+++ java/angular_spring_boot/backend/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200") // Allow Angular app to call this API
 public class HelloController {
 
     @GetMapping("/api/hello")
--- java/angular_spring_boot/frontend/src/app/app.component.ts
+++ java/angular_spring_boot/frontend/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  message = 'Loading...';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('http://localhost:8080/api/hello', { responseType: 'text' })
+      .subscribe(data => {
+        this.message = data;
+      });
+  }
 }
