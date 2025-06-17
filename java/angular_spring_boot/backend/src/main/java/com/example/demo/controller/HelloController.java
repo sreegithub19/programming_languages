6b892f1a58c71916c3338dea5d316af682c247c2
sreegithub19 <cpadmaja2003@gmail.com>
2025-06-17 22:58:43 +0530

    Message

--- java/angular_spring_boot/backend/src/main/java/com/example/demo/controller/HelloController.java
+++ java/angular_spring_boot/backend/src/main/java/com/example/demo/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200") // Allow Angular app to call this API
+@CrossOrigin(origins = "*")
 public class HelloController {
 
     @GetMapping("/api/hello")
