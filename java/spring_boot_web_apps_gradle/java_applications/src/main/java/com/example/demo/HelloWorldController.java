19500b47e135270e70739241df0a91bba590a611
sreegithub19 <cpadmaja2003@gmail.com>
2025-07-26 13:23:57 +0530

    Message

--- java/spring_boot_web_apps_gradle/java_applications/src/main/java/com/example/demo/HelloWorldController.java
+++ java/spring_boot_web_apps_gradle/java_applications/src/main/java/com/example/demo/HelloWorldController.java
@@ -1,94 +1,3 @@
-// package com.example.demo;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-// import java.nio.charset.StandardCharsets;
-// import java.nio.file.Paths;
-// import java.io.IOException;
-// import java.nio.file.Files;
-
-
-// @RestController
-// public class HelloWorldController {
-
-// 	@RequestMapping
-// 	public String helloWorld() throws IOException {
-// 		return new String(Files.readAllBytes(Paths.get("./src/main/resources/static/helloWorld.html")), StandardCharsets.UTF_8);
-// 	}
-
-// 	@RequestMapping("/chess")
-// 	public String chess() throws IOException {
-// 		return new String(Files.readAllBytes(Paths.get("./src/main/resources/static/chess.html")), StandardCharsets.UTF_8);
-// 	}
-
-// 	@RequestMapping("/calculator")
-// 	public String calculator() throws IOException {
-// 		return new String(Files.readAllBytes(Paths.get("./src/main/resources/static/calculator.html")), StandardCharsets.UTF_8);
-// 	}
-
-// 	@RequestMapping("/sass_")
-// 	public String sass_() throws IOException {
-// 		return new String(Files.readAllBytes(Paths.get("./src/main/resources/static/sass_.html")), StandardCharsets.UTF_8);
-// 	}
-
-// 	@RequestMapping("/maze")
-// 	public String maze() throws IOException {
-// 		return new String(Files.readAllBytes(Paths.get("./src/main/resources/static/maze.html")), StandardCharsets.UTF_8);
-// 	}
-
-// 	@RequestMapping("/codepen")
-// 	public String codepen() throws IOException {
-// 		return new String(Files.readAllBytes(Paths.get("./src/main/resources/static/codepen.html")), StandardCharsets.UTF_8);
-// 	}
-
-// 	@RequestMapping("/tilt_maze")
-// 	public String tilt_maze() throws IOException {
-// 		return new String(Files.readAllBytes(Paths.get("./src/main/resources/static/tilt_maze.html")), StandardCharsets.UTF_8);
-// 	}
-
-// 	@RequestMapping("/tic_tac_toe")
-// 	public String tic_tac_toe() throws IOException {
-// 		return new String(Files.readAllBytes(Paths.get("./src/main/resources/static/tic_tac_toe.html")), StandardCharsets.UTF_8);
-// 	}
-
-// 	@RequestMapping("/clock")
-// 	public String clock() throws IOException {
-// 		return new String(Files.readAllBytes(Paths.get("./src/main/resources/static/clock.html")), StandardCharsets.UTF_8);
-// 	}
-
-// 	@RequestMapping("/hangman")
-// 	public String hangman() throws IOException {
-// 		return new String(Files.readAllBytes(Paths.get("./src/main/resources/static/hangman.html")), StandardCharsets.UTF_8);
-// 	}
-
-// 	@RequestMapping("/puzzles")
-// 	public String puzzles() throws IOException {
-// 		return new String(Files.readAllBytes(Paths.get("./src/main/resources/static/puzzles.html")), StandardCharsets.UTF_8);
-// 	}
-
-// 	@RequestMapping("/sudoku")
-// 	public String sudoku() throws IOException {
-// 		return new String(Files.readAllBytes(Paths.get("./src/main/resources/static/sudoku.html")), StandardCharsets.UTF_8);
-// 	}
-
-// 	@RequestMapping("/virtual_keyboard")
-// 	public String virtual_keyboard() throws IOException {
-// 		return new String(Files.readAllBytes(Paths.get("./src/main/resources/static/virtual_keyboard.html")), StandardCharsets.UTF_8);
-// 	}
-
-// 	@RequestMapping("/solitaire")
-// 	public String solitaire() throws IOException {
-// 		return new String(Files.readAllBytes(Paths.get("./src/main/resources/static/solitaire.html")), StandardCharsets.UTF_8);
-// 	}
-
-// 	@RequestMapping("/dino")
-// 	public String dino() throws IOException {
-// 		return new String(Files.readAllBytes(Paths.get("./src/main/resources/static/dino.html")), StandardCharsets.UTF_8);
-// 	}
-
-// }
-
-
-
 package com.example.demo;
 
 import org.springframework.util.StreamUtils;
@@ -99,8 +8,27 @@ import java.io.InputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+
 @RestController
 public class HelloWorldController {
+
+    // 1A
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> pairIdx = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (pairIdx.containsKey(target - num)) {
+                return new int[] { i, pairIdx.get(target - num) };
+            }
+            pairIdx.put(num, i);
+        }
+
+        return new int[] {};        
+    }
 
     private String loadHtml(String filename) throws IOException {
         try (InputStream is = getClass().getClassLoader().getResourceAsStream("static/" + filename)) {
@@ -193,4 +121,29 @@ public class HelloWorldController {
     public String dino() throws IOException {
         return loadHtml("dino.html");
     }
+
+    @RequestMapping("/1A")
+    public String testTwoSum() {
+        int[][] testInputs = {
+                {3, 3},
+                {-1, -2, -3, -4},
+                {1000000000, 1000000000},
+                {1, 5, 7, 9},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+                {0, 4, 3, 0},
+        };
+
+        StringBuilder result = new StringBuilder();
+
+        for (int[] input : testInputs) {
+            int target = input.length > 1 ? input[0] + input[1] : 0;
+            int[] res = twoSum(input, target);
+            result.append("Input: ").append(Arrays.toString(input))
+                  .append(" => Output (target ").append(target).append("): ");
+            result.append(res.length == 0 ? "None" : Arrays.toString(res)).append("<br>");
+        }
+
+        return result.toString();
+    }
+
 }
